2b4b8b5cb6545741aab76d830efcc42908a16f39
parabsunita <parabsunita10397@gmail.com>
2023-01-31 22:32:08 +0530

    latest changes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_URL: "http://54.175.73.149:3000/",
-  CALLBACK_URL: "http://localhost:3001/my-orders",
+  CALLBACK_URL: "http://54.175.73.149:3001/my-orders",
   TIMEOUT: 30000,
   Headers: {
     "Content-Type": "application/json",
